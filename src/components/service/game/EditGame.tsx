86a334b4b07618b8ded21ad82a391a56abee6854
UdanDias <udandias892@gmail.com>
2026-02-23 08:25:07 +0530

    added form elements

--- src/components/service/game/EditGame.tsx
+++ src/components/service/game/EditGame.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { Modal, FloatingLabel, Form, Button } from "react-bootstrap";
 interface Game{
     gameId:string;
@@ -6,10 +7,10 @@ interface Game{
     score1:number;
     score2:number;
     margin:number;
-    isgameTied:boolean;
+    isgameTied:string;
     winnerId:string;
     gameDate:string;
-    isByeGame:boolean;
+    isByeGame:string;
 
 }
 
@@ -21,127 +22,136 @@ interface GameEditProps{
     refreshTable:()=>void;
 }
 function EditGame({show,selectedRow,handleClose,handleUpdate,refreshTable}:GameEditProps){
+    const [gameData,SetGameData]=useState<Game>({
+        gameId:"",
+        player1Id:"",
+        player2Id:"",
+        score1:0,
+        score2:0,
+        margin:0,
+        isgameTied:"",
+        winnerId:"",
+        gameDate:"",
+        isByeGame:""
+    })
+    useEffect(()=>{
+        if(selectedRow){
+            SetGameData(selectedRow)
+        }
+        
+    },[selectedRow])
+    const handleOnChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        SetGameData({...gameData,[e.target.name]:e.target.value})
+
+    }
+    const handleSave=(updatedGame:Game)=>{
+
+    }
     return(
         <>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                <Modal.Title>Edit Player</Modal.Title>
+                <Modal.Title>Edit Game</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <FloatingLabel controlId="floatingInput" label="Player Id" className="mb-3">
+                    <FloatingLabel controlId="floatingInput" label="Game Id" className="mb-3">
                         <Form.Control 
                         readOnly
                         type="text" 
-                        name="playerId" 
-                        placeholder="Player Id"
-                        value={playerDetails.playerId}
+                        name="gameId" 
+                        placeholder="Game Id"
+                        value={gameData.gameId}
                         onChange={handleOnChange} />
                     </FloatingLabel>
                     
-                    <FloatingLabel controlId="floatingInput" label="First Name" className="mb-3">
+                    <FloatingLabel controlId="floatingInput" label="Player 1 Id" className="mb-3">
                         <Form.Control 
                         type="text" 
-                        name="firstName" 
-                        placeholder="First Name"
-                        value={playerDetails.firstName}
+                        name="playe1Id" 
+                        placeholder="Player 1 Id"
+                        value={gameData.player1Id}
                         onChange={handleOnChange} />
                     </FloatingLabel>
 
-                    <FloatingLabel controlId="floatingInput" label="Last Name" className="mb-3">
+                    <FloatingLabel controlId="floatingInput" label="Player 2 Id" className="mb-3">
                         <Form.Control 
                         type="text" 
-                        name="lastName" 
-                        placeholder="Last Name"
-                        value={playerDetails.lastName}
+                        name="player2Id" 
+                        placeholder="Player 2 Id"
+                        value={gameData.player2Id}
                         onChange={handleOnChange} />
                     </FloatingLabel>
                     
-                    <FloatingLabel controlId="floatingInput" label="Age" className="mb-3">
+                    <FloatingLabel controlId="floatingInput" label="Score 1" className="mb-3">
                         <Form.Control 
                         type="number" 
-                        name="age" 
-                        placeholder="Age"
-                        value={playerDetails.age}
+                        name="score1" 
+                        placeholder="Score 1"
+                        value={gameData.score1}
                         onChange={handleOnChange} />
                     </FloatingLabel>
 
-                    <FloatingLabel controlId="floatingInput" label="Gender" className="mb-3">
+                    <FloatingLabel controlId="floatingInput" label="Score 2" className="mb-3">
                         <Form.Control 
-                        type="text" 
-                        name="gender" 
-                        placeholder="Gender" 
-                        value={playerDetails.gender}
+                        type="number" 
+                        name="score2" 
+                        placeholder="Score 2" 
+                        value={gameData.score2}
                         onChange={handleOnChange}/>
                     </FloatingLabel>
                     
-                    <FloatingLabel controlId="floatingInput" label="Date of Birth" className="mb-3">
+                    <FloatingLabel controlId="floatingInput" label="margin" className="mb-3">
+                        <Form.Control 
+                        type="number" 
+                        name="margin"
+                        placeholder="Margin" 
+                        value={gameData.margin}
+                        onChange={handleOnChange}/>
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingInput" label="Is Game Tied" className="mb-3">
+                        <Form.Control 
+                        type="text" 
+                        name="isGameTied"
+                        placeholder="Is Game Tied"
+                        value={gameData.isgameTied}
+                        onChange={handleOnChange} />
+                    </FloatingLabel>
+                    
+                    <FloatingLabel controlId="floatingInput" label="Winner Id" className="mb-3">
+                        <Form.Control 
+                        
+                        type="text" 
+                        name="winnerId"
+                        placeholder="Winner Id"
+                        value={gameData.winnerId}
+                        onChange={handleOnChange}/>
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingInput" label="Game Date" className="mb-3">
                         <Form.Control 
                         type="date" 
-                        name="dob"
-                        placeholder="Date of Birth" 
-                        value={playerDetails.dob}
-                        onChange={handleOnChange}/>
-                    </FloatingLabel>
-
-                    <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
-                        <Form.Control 
-                        type="text" 
-                        name="email"
-                        placeholder="Email"
-                        value={playerDetails.email}
+                        name="gameDate"
+                        placeholder="Game Date"
+                        value={gameData.gameDate}
                         onChange={handleOnChange} />
                     </FloatingLabel>
                     
-                    <FloatingLabel controlId="floatingInput" label="Phone" className="mb-3">
+                    <FloatingLabel controlId="floatingInput" label="Is Game a Bye" className="mb-3">
                         <Form.Control 
                         type="text" 
-                        name="phone"
-                        placeholder="Phone"
-                        value={playerDetails.phone}
-                        onChange={handleOnChange}/>
-                    </FloatingLabel>
-
-                    <FloatingLabel controlId="floatingInput" label="Address" className="mb-3">
-                        <Form.Control 
-                        type="text" 
-                        name="address"
-                        placeholder="Address"
-                        value={playerDetails.address}
-                        onChange={handleOnChange} />
-                    </FloatingLabel>
-                    
-                    <FloatingLabel controlId="floatingInput" label="Faculty" className="mb-3">
-                        <Form.Control 
-                        type="text" 
-                        name="faculty"
-                        placeholder="Faculty"
-                        value={playerDetails.faculty}
+                        name="isByeGame"
+                        placeholder="Is Game a Bye"
+                        value={gameData.isByeGame}
                         onChange={handleOnChange} />
                     </FloatingLabel>
 
-                    <FloatingLabel controlId="floatingInput" label="Academic Level" className="mb-3">
-                        <Form.Control 
-                        type="text" 
-                        name="academicLevel"
-                        placeholder="Academic Level" 
-                        value={playerDetails.academicLevel}
-                        onChange={handleOnChange}/>
-                    </FloatingLabel>
-                    
-                    <FloatingLabel controlId="floatingInput" label="Account Created Date" className="mb-3">
-                        <Form.Control 
-                        type="date" 
-                        name="accountCreatedDate"
-                        placeholder="Account Created Date"
-                        value={playerDetails.accountCreatedDate}
-                        onChange={handleOnChange} />
-                    </FloatingLabel>
                 </Modal.Body>
                 <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     Close
                 </Button>
-                <Button variant="primary" onClick={handlesave}>
+                <Button variant="primary" onClick={handleSave}>
                     Update
                 </Button>
                 </Modal.Footer>
