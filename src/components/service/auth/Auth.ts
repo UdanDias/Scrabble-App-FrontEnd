c1d7bd68148a9b4628d2f045b2c903cea69eae8a
UdanDias <udandias892@gmail.com>
2026-02-25 16:20:47 +0530

    Created the SignIn api call

--- src/components/service/auth/Auth.ts
+++ src/components/service/auth/Auth.ts
@@ -12,6 +12,18 @@ const SignUpTask=async(signUp:any)=>{
         console.error("Error while sending signUp data ",error)
         throw error;
     }
-
 }
-export default SignUpTask;
+
+const SignInTask=async(signIn:any)=>{
+    console.log(signIn);
+    try {
+        const signInResponse= await axios.post(
+            `${baseAuthUrl}/signin`,signIn
+        )
+        return signInResponse.data.token
+    } catch (error) {
+        console.error("Error while sending signIn data ",error)
+        throw error;
+    }
+}
+export default {SignUpTask,SignInTask};
