e4eb6fd182a93e145419df656fffb5a69f0d4403
UdanDias <udandias892@gmail.com>
2026-02-25 14:39:45 +0530

    Created sign in form

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,7 +1,45 @@
+import { Form, Button } from "react-bootstrap";
+
 export const SignIn=()=>{
     return (
         <>
-        <h1>hello signin</h1>
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+            <div className="d-flex flex-column align-items-center ">
+                <h1> Login</h1>
+            </div>
+                <Form className="d-flex flex-column align-items-center mt-3 w-100">
+                    <div className="w-25">
+                        
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control 
+                            type="email" 
+                            placeholder="Enter Email" 
+                            name="email"
+                            />
+                            
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control 
+                            type="email" 
+                            placeholder="Enter email" 
+                            name="password"
+                            />
+                            
+                        </Form.Group>
+
+                        <div className="d-flex justify-content-center">
+                            <Button variant="success" type="submit">
+                                Login
+                            </Button>
+                        </div>
+                        
+                    </div>
+                </Form>
+            </div>
+        
+
         </>
     );
 }
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -3,26 +3,63 @@ import { Form, Button } from "react-bootstrap";
 export const SignUp=()=>{
     return (
         <>
-            <h1>hello signup</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+        <div className="d-flex flex-column align-items-center mt-5">
+            <h1> Sign Up</h1>
+        </div>
+            <Form className="d-flex flex-column align-items-center mt-3">
+                <div className="w-25">
+                    <Form.Group className="mb-3" controlId="firstname">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control 
+                        type="text" 
+                        placeholder="Enter First Name" 
+                        name="firstname"
+                        />
+                        
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="lastname">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control 
+                        type="text" 
+                        placeholder="Enter Last Name" 
+                        name="lastname"
+                        />
+                        
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control 
+                        type="email" 
+                        placeholder="Enter Email" 
+                        name="email"
+                        />
+                        
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control 
+                        type="email" 
+                        placeholder="Enter email" 
+                        name="password"
+                        />
+                        
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                    <Form.Group className="mb-3" controlId="role">
+                        <Form.Label>Role</Form.Label>
+                        <Form.Control 
+                        type="text" 
+                        placeholder="Role" 
+                        name="role"
+                        />
+                    </Form.Group>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="success" type="submit">
+                            Register
+                        </Button>
+                    </div>
+                    
+                </div>
             </Form>
 
         </>
