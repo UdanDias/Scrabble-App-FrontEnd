18b77b8e3038ed8015ea2ad07ec0c474873ba052
UdanDias <udandias892@gmail.com>
2026-03-26 17:11:43 +0530

    Fix auth URL

--- src/components/service/auth/Auth.ts
+++ src/components/service/auth/Auth.ts
@@ -1,29 +1,30 @@
 import axios from "axios";
 import BASE_URL from "../../../config";
 
-const baseAuthUrl=`${BASE_URL}`;
-export const SignUpTask=async(signUp:any)=>{
+const baseAuthUrl = `${BASE_URL}/auth`;
+
+export const SignUpTask = async (signUp: any) => {
     console.log(signUp);
     try {
-        const signUpResponse= await axios.post(
-            `${baseAuthUrl}/signup`,signUp
+        const signUpResponse = await axios.post(
+            `${baseAuthUrl}/signup`, signUp
         )
         return signUpResponse.data.token
     } catch (error) {
-        console.error("Error while sending signUp data ",error)
+        console.error("Error while sending signUp data ", error)
         throw error;
     }
 }
 
-export const SignInTask=async(signIn:any)=>{
+export const SignInTask = async (signIn: any) => {
     console.log(signIn);
     try {
-        const signInResponse= await axios.post(
-            `${baseAuthUrl}/signin`,signIn
+        const signInResponse = await axios.post(
+            `${baseAuthUrl}/signin`, signIn
         )
         return signInResponse.data.token
     } catch (error) {
-        console.error("Error while sending signIn data ",error)
+        console.error("Error while sending signIn data ", error)
         throw error;
     }
 }
